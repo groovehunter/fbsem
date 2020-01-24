c420bdd39cd244897dfb62fcf874d83ec76705bb
Florian Konnertz <sstk@posteo.de>
2020-01-24 12:20:53 +0100

    change to mysql

--- fbsem/settings.py
+++ fbsem/settings.py
@@ -62,8 +62,10 @@ WSGI_APPLICATION = 'fbsem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_fbsem',
+        'USER': 'django',
+        'PASSWORD': 'django',
     }
 }
 
